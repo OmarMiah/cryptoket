c722466eae3f3553a27dfb621566f8ea478f63e8
OmarMiah <omiahcs@gmail.com>
2023-03-18 09:33:33 -0400

    add routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import '../styles/globals.css';
 
 // eslint-disable-next-line react/function-component-definition
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+
+const MyApp = ({ Component, pageProps }) => (
+  <div>
+    <h1>NAVBAR</h1>
+    <Component {...pageProps} />
+    <h1>FOOTER</h1>
+  </div>
+);
+export default MyApp;
