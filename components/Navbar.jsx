2adca6ff4e5d9f7c66054b30320fff8ecacbe8dc
OmarMiah <omiahcs@gmail.com>
2023-03-18 22:00:27 -0400

    add navs and handle light mode

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,6 +5,30 @@ import Image from 'next/image';
 import Link from 'next/link';
 import images from '../assets';
 
+const MenuItems = (isMobile) => {
+  const generateLink = () => {};
+
+  return (
+    <ul
+      className={`list-none flexCenter flex-row ${
+        isMobile && 'flex-col h-full'
+      }`}
+    >
+      {['Explore NFTs', 'Listed NFTs', 'My NFTs'].map((item, i) => (
+        <li
+          key={i}
+          onClick={() => {}}
+          className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3 ${
+            active === item
+              ? 'dark:text-white text-nft-black-1'
+              : 'dark:text-nft-gray-3 text-nft-gray-2'
+          }`}
+        />
+      ))}
+    </ul>
+  );
+};
+
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
 
@@ -12,8 +36,57 @@ const Navbar = () => {
     <nav className='flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1'>
       <div>
         <Link href='/'>
-          <div />
+          <div
+            className='flexCenter md:hidden cursor-pointer'
+            onClick={() => {}}
+          >
+            <Image
+              src={images.logo02}
+              objectfit='contain'
+              width={32}
+              height={32}
+              alt='logo'
+            />
+            <p className='dark:text-white text-nft-black-1 font-semibold text-lg ml-1'>
+              CryptoKet
+            </p>
+          </div>
         </Link>
+        <Link href='/'>
+          <div className='hidden md:flex' onClick={() => {}}>
+            <Image
+              src={images.logo02}
+              objectfit='contain'
+              width={32}
+              height={32}
+              alt='logo'
+            />
+          </div>
+        </Link>
+      </div>
+      <div className='flex flex-initial flex-row justify-end'>
+        <div className='flex items-center mr-2'>
+          <input
+            type='checkbox'
+            className='checkbox'
+            id='checkbox'
+            onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          />
+          <label
+            htmlFor='checkbox'
+            className='flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label'
+          >
+            <i className='fas fa-sun' />
+            <i className='fas fa-moon' />
+            <div className='w-3 h-3 absolute bg-white rounded-full ball' />
+          </label>
+        </div>
+      </div>
+
+      <div className='md:hidden flex'>
+        <ul className='list-none flexCenter flex-row'>
+          <MenuItems />
+        </ul>
       </div>
     </nav>
   );
