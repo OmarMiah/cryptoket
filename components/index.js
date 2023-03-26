4912b4c8130854660d429abacfc3a56f51f98c73
OmarMiah <omiahcs@gmail.com>
2023-03-25 20:09:14 -0400

    upload file

--- components/index.js
+++ components/index.js
@@ -3,5 +3,6 @@ import Footer from './Footer';
 import Button from './Button';
 import Banner from './Banner';
 import CreatorCard from './CreatorCard';
+import NFTCard from './NFTCard';
 
-export { Navbar, CreatorCard, Footer, Button, Banner };
+export { Navbar, CreatorCard, Footer, Button, Banner, NFTCard };
